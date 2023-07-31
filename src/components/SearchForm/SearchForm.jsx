2fa6a60ddb6c0e8aef2a9e36c32bfc3d6a8cd2bf
alexgoit <alexandrpuryaev@gmail.com>
2023-07-31 16:35:25 +0300

    Add SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,11 +1,28 @@
 import { Button, Input } from '@mui/material';
-import { Form } from 'react-router-dom';
+import Loader from 'components/Loader';
+import { useFetchMovie } from 'hooks/useFetchMovie';
+import { useSearchParams } from 'react-router-dom';
 
 export default function SearchForm() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const query = form.elements.query.value;
+
+    setSearchParams({ query });
+    form.reset();
+  };
+
   return (
-    <Form>
-      <Input />
-      <Button type="submit">Search</Button>
-    </Form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" name="query" placeholder="Search..." />
+        <Button type="submit">Search</Button>
+      </form>
+      <Loader open={false} />
+    </>
   );
 }
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,12 @@
 import { Container } from '@mui/system';
-// import SearchForm from 'components/SearchForm';
+import SearchForm from 'components/SearchForm';
 
 export default function Movies() {
   return (
     <>
       <Container sx={{ pt: 2, pb: 2 }}>
         <h2>Movies</h2>
-        {/* <SearchForm /> */}
+        <SearchForm />
       </Container>
     </>
   );
