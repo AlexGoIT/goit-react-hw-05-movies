6c07300c77b886e7772c53ea19deb56b8584be9c
alexgoit <alexandrpuryaev@gmail.com>
2023-07-31 09:29:59 +0300

    Add MovieDetails styles

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,38 +1,56 @@
-import { Box, Container, List, Typography } from '@mui/material';
-import Loader from 'components/Loader';
 import { Outlet, useParams } from 'react-router-dom';
-import { Poster, StyledLink } from './MovieDetails.styled';
 import { useFetchMovie } from 'hooks/useFetchMovie';
+import { Box, Container, List } from '@mui/material';
+import Loader from 'components/Loader';
+import {
+  Thumb,
+  Poster,
+  NoPoster,
+  MovieTitle,
+  MovieScore,
+  MovieSubTitle,
+  MovieReview,
+  MovieGenre,
+  StyledLink,
+} from './MovieDetails.styled';
 
 export default function MoviesDetails() {
   const { movieId } = useParams();
   const [movie, open, error] = useFetchMovie(`/movie/${movieId}`);
   const imgSrc = movie?.poster_path
     ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
-    : '';
+    : null;
   const voteAverage = Math.floor(movie?.vote_average * 10);
 
   return (
     <>
       {!error && (
-        <Container>
-          <Box sx={{ mt: 2, display: 'flex' }}>
-            <Poster
-              src={imgSrc}
-              alt={movie?.title ?? movie?.name}
-              loading="lazy"
-            ></Poster>
+        <Container sx={{ pt: 2 }}>
+          <Box sx={{ display: 'flex' }}>
+            <Thumb>
+              {imgSrc ? (
+                <Poster
+                  src={imgSrc}
+                  alt={movie?.title ?? movie?.name}
+                  loading="lazy"
+                ></Poster>
+              ) : (
+                <NoPoster />
+              )}
+            </Thumb>
             <Box>
-              <Typography variant="h4" component="h2">
-                {movie?.title ?? movie?.name}
-              </Typography>
-              <div>
+              <MovieTitle>{movie?.title ?? movie?.name}</MovieTitle>
+              <MovieScore>
                 User score: {voteAverage ? `${voteAverage}%` : 'No score'}
-              </div>
-              <div>Review</div>
-              <div>{movie?.overview ? movie?.overview : 'No overview'}</div>
-              <div>Genre</div>
-              <div>{movie?.genres.map(({ name }) => name).join(', ')}</div>
+              </MovieScore>
+              <MovieSubTitle>Review</MovieSubTitle>
+              <MovieReview>
+                {movie?.overview ? movie?.overview : 'No overview'}
+              </MovieReview>
+              <MovieSubTitle>Genre</MovieSubTitle>
+              <MovieGenre>
+                {movie?.genres.map(({ name }) => name).join(', ')}
+              </MovieGenre>
             </Box>
           </Box>
 
--- src/components/MovieDetails/MovieDetails.styled.js
+++ src/components/MovieDetails/MovieDetails.styled.js
@@ -1,14 +1,54 @@
 import { NavLink } from 'react-router-dom';
+import { BsFilm } from 'react-icons/bs';
 import styled from 'styled-components';
+
+export const Thumb = styled.div`
+  min-width: 260px;
+  min-height: 360px;
+  margin-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 1px 2px 5px 0px rgba(34, 60, 80, 0.2);
+`;
 
 export const Poster = styled.img`
   max-width: 260px;
-  height: 360px;
-  margin-right: 20px;
+  height: auto;
   object-fit: cover;
   object-position: center;
   background-color: bisque;
 `;
+
+export const NoPoster = styled(BsFilm)`
+  width: calc(100% - 150px);
+  height: auto;
+  object-fit: cover;
+  color: #c2c2c2;
+`;
+
+export const MovieTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const MovieScore = styled.p`
+  margin-block: 16px;
+  font-size: 18px;
+`;
+
+export const MovieSubTitle = styled.p`
+  margin-bottom: 4px;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const MovieReview = styled.p`
+  margin-bottom: 16px;
+  text-align: justify;
+`;
+
+export const MovieGenre = styled.p``;
 
 export const StyledLink = styled(NavLink)`
   font-size: 18px;
